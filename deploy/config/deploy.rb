e30eb38a974e1b87f5cc006d7938c3ecc303ddc1
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2018-02-15 11:18:26 +0100

    add deploy script

---
+++ deploy/config/deploy.rb
@@ -0,0 +1,58 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.0"
+
+set :application, "weareclutch"
+set :repo_url, "git@github.com:weareclutch/frontend.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/elm/weareclutch"
+set :temp_dir, "/home/elm/tmp"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+
+namespace :deploy do
+  desc 'Deploy latest code'
+
+  task :build do
+    run_locally do
+      execute 'yarn build'
+    end
+
+    on roles(:all) do |host|
+      upload! '../public/app.js', "#{current_path}/public"
+    end
+  end
+
+  after :publishing, :build
+end
+
