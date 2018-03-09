d3f2f6cc4ef6127b0c71758a6ac429e8cd9a18dd
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2018-03-09 15:50:40 +0100

    add staging url

--- deploy/config/deploy/staging.rb
+++ deploy/config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server "staging.weareclutch.nl", user: "elm", roles: %{app}
 
 
 # role-based syntax
