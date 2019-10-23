b6ff38d5785bffc6f92b995078213381bed80c7c
Muchu Yu <muchu.yu@gmail.com>
2019-10-23 11:01:42 +0200

    Reduces image height for mobile to compromise tool bar

--- src/UI/Pages/Home.elm
+++ src/UI/Pages/Home.elm
@@ -119,7 +119,7 @@ introCover content logoIndex =
                     wrapper =
                         styled div
                             [ width (pct 100)
-                            , height (vh 95)
+                            , height (vh 90)
                             , backgroundColor (hex bgColor)
                             , position relative
                             , whiteSpace normal
